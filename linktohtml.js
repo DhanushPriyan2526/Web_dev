7d8a83e2481a84aa89ab82065586b02612a58a3d
Dhanush Priyan <dhanushdp2526@gmail.com>
2023-08-03 16:40:39 +0530

    js added

---
+++ linktohtml.js
@@ -0,0 +1,6 @@
+function First(){
+    document.getElementById("new").innerHTML="Im in First Year"
+}
+function Second(){
+    document.getElementById("new").innerHTML="Im in second Year"
+}
--- script.js
+++ script.js
@@ -28,3 +28,26 @@ array.forEach(element => console.log(element))
 for (i=0 ;i<array.length;i++){
     console.log(array[i])
 }
+
+var array1=[1,2,3,4,5]
+array1.push(23)
+for (var ele of array1) {
+    console.log(ele)
+}
+
+array1.pop()
+console.log(array1)
+
+
+// object
+a={
+    name:"Batman",
+    Age:35,
+    Moto:"Im Batman",
+    Profession:"Millionaire"
+}
+a["code"]="DC"
+console.log(a)
+for(var ele in a){
+    console.log("values : "+a[ele])
+}
